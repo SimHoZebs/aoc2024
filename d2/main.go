d5883f298ef87c0a1d4c7092c1854043e790c446
SimHoZebs <simhozebs@gmail.com>
2024-12-08 00:40:27 -0500

    d2 p2

--- d2/main.go
+++ d2/main.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -32,8 +33,7 @@ func part1() {
 			diffSign := math.Signbit(diff)
 			absDiff := math.Abs(diff)
 
-			if (absDiff > 3 || absDiff < 1) ||
-				(i > 1 && prevDiffSign != diffSign) {
+			if (absDiff > 3 || absDiff < 1) || (i > 1 && prevDiffSign != diffSign) {
 				isSafe = false
 				break
 			}
@@ -49,4 +49,71 @@ func part1() {
 
 	}
 	fmt.Printf("part1: %d\n", ans)
+}
+
+func part2() {
+	file, _ := os.Open("input")
+	defer file.Close()
+
+	var ans int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := strings.Fields(scanner.Text())
+		// fmt.Println(line)
+
+		// if safe, i, j := isSafe(line, -1); safe {
+		// 	ans++
+		// } else if safe, _, _ := isSafe(line, i); safe {
+		// 	ans++
+		// } else if safe, _, _ := isSafe(line, j); safe {
+		// 	ans++
+		// } else {
+		// 	fmt.Println("My attempt deems it Not Safe")
+		// }
+
+		for i := -1; i < len(line); i++ {
+			if safe, _, _ := isSafe(line, i); safe {
+				ans++
+				break
+			}
+		}
+	}
+
+	fmt.Printf("part2: %d\n", ans)
+}
+
+func isSafe(arr []string, skip int) (bool, int, int) {
+	var prevDiff = math.Inf(1)
+	isSafe := true
+	var i, j = 0, 1
+	for j < len(arr) {
+
+		if j == skip {
+			j++
+			continue
+		} else if i == skip {
+			i++
+			j++
+			continue
+		}
+
+		level1, _ := strconv.Atoi(arr[i])
+		level2, _ := strconv.Atoi(arr[j])
+
+		diff := float64(level1 - level2)
+		diffSign := math.Signbit(diff)
+		prevDiffSign := math.Signbit(prevDiff)
+		absDiff := math.Abs(diff)
+
+		if (absDiff > 3 || absDiff < 1) || (prevDiff != math.Inf(1) && prevDiffSign != diffSign) {
+			isSafe = false
+			break
+		}
+
+		i = j
+		j++
+		prevDiff = diff
+	}
+
+	return isSafe, i, j
 }
